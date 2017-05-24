05725cb44ee2d8df1862e8677b41ea889a759cce
nic <nic@nybbl.co>
2017-05-24 08:56:38 +0100

    Initial commit.

---
+++ src/Mustache.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nic
+ * Date: 24/05/2017
+ * Time: 08:52
+ */
+
+namespace Slim\Views;
+
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Class Mustache
+ * @package Slim\Views
+ */
+class Mustache
+{
+    /** @var \Mustache_Engine $mustache */
+    protected $mustache;
+
+    /**
+     * Mustache constructor.
+     * @param array $options
+     */
+    public function __construct(array $options = [])
+    {
+        $this->setMustache(new \Mustache_Engine($options));
+    }
+
+    /**
+     * Returns the rendered mustache template.
+     *
+     * @param string $template
+     * @param array $data
+     * @return string
+     */
+    public function fetch($template, array $data)
+    {
+        return $this->getMustache()->render($template, $data);
+    }
+
+    /**
+     * Renders the template with PSR7 Response object.
+     *
+     * @param ResponseInterface $response
+     * @param $template
+     * @param array $data
+     * @return int
+     */
+    public function render(ResponseInterface $response, $template, array $data = [])
+    {
+        return $response->getBody()->write($this->fetch($template, $data));
+    }
+
+    # ---------------------------------------------------------------
+    # - GETTERS AND SETTERS
+    # ---------------------------------------------------------------
+
+    /**
+     * @return \Mustache_Engine
+     */
+    public function getMustache()
+    {
+        return $this->mustache;
+    }
+
+    /**
+     * @param \Mustache_Engine $mustache
+     */
+    protected function setMustache($mustache)
+    {
+        $this->mustache = $mustache;
+    }
+}
